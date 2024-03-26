caa8fb2fbada48b1f790f25f006f4435d402f935
ChaeDoll <dlacodbs0627@gmail.com>
2024-03-26 10:05:21 +0900

    240326

--- src/main/java/com/capstone/feelcheck/controller/CommentController.java
+++ src/main/java/com/capstone/feelcheck/controller/CommentController.java
@@ -1,11 +1,13 @@
 package com.capstone.feelcheck.controller;
 
 import com.capstone.feelcheck.dto.CommentDto;
+import com.capstone.feelcheck.dto.ResponseDto;
 import com.capstone.feelcheck.model.Comment;
 import com.capstone.feelcheck.model.User;
 import com.capstone.feelcheck.repository.CommentRepository;
 import com.capstone.feelcheck.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,16 +19,16 @@ public class CommentController {
     private UserRepository userRepository;
 
     @PostMapping("/api/comments")
-    public ResponseEntity<?> postComment(@RequestBody CommentDto commentDto){
+    public ResponseDto postComment(@RequestBody CommentDto commentDto){
         User user = userRepository.findByNickname(commentDto.getNickname());
         if (user == null){
-            return ResponseEntity.badRequest().body("닉네임 '" + commentDto.getNickname() + "' 이 존재하지 않습니다.");
+            return new ResponseDto(HttpStatus.UNAUTHORIZED.value(), "닉네임 '" + commentDto.getNickname() + "' 이 존재하지 않습니다.");
         }
         Comment comment = new Comment();
         comment.setUser(user);
         comment.setEmotion(commentDto.getEmotion());
         comment.setComment(commentDto.getComment());
         commentRepository.save(comment);
-        return ResponseEntity.ok("댓글이 성공적으로 작성되었습니다.");
+        return new ResponseDto(HttpStatus.OK.value(), "댓글이 성공적으로 작성되었습니다.");
     }
 }
--- src/main/java/com/capstone/feelcheck/controller/UserController.java
+++ src/main/java/com/capstone/feelcheck/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.capstone.feelcheck.controller;
 
+import com.capstone.feelcheck.dto.ResponseDto;
 import com.capstone.feelcheck.model.Comment;
 import com.capstone.feelcheck.model.User;
 import com.capstone.feelcheck.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,12 +17,14 @@ public class UserController {
     private UserRepository userRepository;
 
     @PostMapping("/api/users")
-    public ResponseEntity<?> userCreate(@RequestBody User user){
+    public ResponseDto userCreate(@RequestBody User user){
         Boolean nicknameCheck = (userRepository.findByNickname(user.getNickname())) == null;
         if (!nicknameCheck){
-            return ResponseEntity.badRequest().body("이미 존재하는 닉네임입니다.");
+            return new ResponseDto(HttpStatus.UNAUTHORIZED.value(), "이미 존재하는 닉네임입니다.");
+//            return ResponseEntity.badRequest().body("이미 존재하는 닉네임입니다.");
         }
         userRepository.save(user);
-        return ResponseEntity.ok("닉네임이 등록되었습니다.");
+        return new ResponseDto(HttpStatus.OK.value(), "닉네임이 등록되었습니다.");
+//        return ResponseEntity.ok("닉네임이 등록되었습니다.");
     }
 }
